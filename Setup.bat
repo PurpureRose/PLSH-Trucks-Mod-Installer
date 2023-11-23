6e0641ae64a99ac34b3905fc3e6ef73dc019780d
Evo_red <148472951+PurpureRose@users.noreply.github.com>
2023-11-23 17:39:26 +0300

    Updated Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -3,7 +3,6 @@
 set PYTHON_VERSION=3.11.5
 set PYTHON_INSTALLER_DIR=resources\installer
 set PYTHON_INSTALLER=%PYTHON_INSTALLER_DIR%\python-%PYTHON_VERSION%.exe
-set PYTHON_LIBS_INSTALLER = installibs.bat
 
 echo Installing Python %PYTHON_VERSION%, it may take a while...
 
@@ -13,7 +12,7 @@ start /wait %PYTHON_INSTALLER% /quiet InstallAllUsers=1 PrependPath=1
 echo Python installation complete.
 
 rem Call libsinstaller
-call %PYTHON_INSTALLER_DIR%\%PYTHON_LIBS_INSTALLER%
+call %PYTHON_INSTALLER_DIR%\installibs.bat
 
 rem Pause to keep the command prompt window open
 pause
